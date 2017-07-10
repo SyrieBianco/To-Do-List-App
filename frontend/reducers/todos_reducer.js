5a96cb17d0f2bce82bf87161eb46dd20c8e3a83a
Syrie Bianco <SyrieBianco@gmail.com>
2017-07-10 14:07:58 -0700

    Complete phase 2

--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,17 +1,35 @@
 import { RECEIVE_TODOS, RECEIVE_TODO} from "../actions/actions";
+import merge from 'lodash/merge';
 
-const todosReducer = (state = {}, action) => {
+const initialState = {
+  1: {
+    id: 1,
+    title: "wash car",
+    body: "with soap",
+    done: false
+  },
+  2: {
+    id: 2,
+    title: "wash dog",
+    body: "with shampoo",
+    done: true
+  },
+};
+
+const todosReducer = (state = initialState, action) => {
   Object.freeze(state);
 
   switch(action.type){
     case RECEIVE_TODO:
-      return {
-
-      };
+      let newState = merge({}, state);
+      newState[action.todo.id] = action.todo;
+      return newState;
     case RECEIVE_TODOS:
-      return {
-
-      };
+      let newstate = {};
+      action.todos.forEach(todo => {
+        newstate[todo.id] = todo;
+      });
+      return newstate;
     default:
       return state;
   }
